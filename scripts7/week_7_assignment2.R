c3517fd1378b8cc2644b4c12d9932500a89569e0
Your Name <eki2@uw.edu>
2018-05-08 13:55:27 -0700

    filtered out all scale lengths <1

--- scripts7/week_7_assignment2.R
+++ scripts7/week_7_assignment2.R
@@ -1,6 +1,12 @@
 #Week 7 assignment 
 
+#reads in the data file and names it "fish_data_cat"
 fish_data_cat <- read.csv("data7/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
+
+View(fish_data_cat)
+
+fish_data_cat_2 <- filter(fish_data_cat, scalelength > 1)
+View(fish_data_cat_2)
 
